e348fb0d29439ad9718047a39cc0cd4be65262de
Rafael Pires Gonçalves <127984148+Rafael1572008@users.noreply.github.com>
2024-10-16 15:08:15 -0300

    retifications

--- controllers/controller1.py
+++ controllers/controller1.py
@@ -1,20 +1,29 @@
 from flask import Blueprint, render_template, session, request, url_for, redirect
-from models.models1 import tarefas
+from models.models1 import tarefas   #importaçãoes
 
-tarefas_controllers = Blueprint('tarefas', __name__)
 
-@tarefas_controllers.route('/')
+tarefas_controllers = Blueprint('tarefas', __name__) #blueprint vindo de models, necessario para fazer a configuração
+
+
+@tarefas_controllers.route('/')  #aba principal
 def index():
     if 'username' in session:
+        print (f"Bem Vindo {session['username']}")
         return render_template('html.html', tarefas = tarefas)
-    return redirect(url_for('tarefas.hello'))
+    return redirect(url_for('tarefas.login'))
 
 
-@tarefas_controllers.route('/hello')
-def hello():
-    return render_template("login.html")
+@tarefas_controllers.route('/login')   #login
+def login():
+    return render_template("login.html", aviso = False)
 
-@tarefas_controllers.route('/pegar', methods = ['POST'])
+
+tarefas_controllers.before_request   #não compreendi a onde aparece
+def request_info():
+    print("execulta antes da requisição")
+
+
+@tarefas_controllers.route('/pegar', methods = ['POST', 'GET'])   ##rota para obter os dados
 def pegar():
     nome = request.form.get('username')
     senha = request.form.get('password')
@@ -22,4 +31,11 @@ def pegar():
     for user in tarefas:
         if user["nome"] == nome and user["senha"] == senha:
             session['username'] = nome
-            return render_template('html.html', tarefas = tarefas)
+            return redirect(url_for('tarefas.index'))
+        
+        aviso = "Senha ou usuario incorreto"
+        return render_template('login.html', aviso = aviso)
+    
+
+#perguntar a onde se aplica o tempo de sessão
+#não entendi midware
