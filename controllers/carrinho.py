c1426a551ce34a59e6615d6b5226a8706cba28ea
Rafael Pires Gonçalves <127984148+Rafael1572008@users.noreply.github.com>
2024-10-23 10:37:04 -0300

    Meio do cookie

--- controllers/carrinho.py
+++ controllers/carrinho.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, session, request, url_for, redirect, make_response
-from models.produtos import produtos   # Importações
+from flask import Blueprint, render_template, request, url_for, redirect, make_response
+from models.produtos import produtos  # Importações
 
 prod = Blueprint('produtos', __name__)
 
@@ -14,26 +14,22 @@ def add():
     elif request.method == 'GET':
         produto_id = request.args.get('produto_id')
 
-    for i in produtos:
-        print(f"Produtos(id={i.id}, nome={i.nome}, preco={i.preco}, img={i.img})")
+    # Redireciona para a rota /set_cookie com o ID do produto
+    return redirect(url_for('produtos.set_cookie', produto_id=produto_id))
 
-    return redirect(url_for('produtos.carr'))
-
-@prod.route('/set_cookie')
-def cookie(produto_id):
-
+@prod.route('/set_cookie/<int:produto_id>')
+def set_cookie(produto_id):
+    # Busca o produto na lista de produtos
     for i in produtos:
         if i.id == produto_id:
-            nome = i.nome
-            preco = i.preco
-            img = i.img
-    
-    valor = f"Cód. Do produto: {produto_id}, nome: {nome}, preco: {preco}, img: {img}"
+            produto = i
+            break 
 
-    resp = make_response("Cookie has been set")
-    resp.set_cookie('car', valor, max_age=60*60*24)  # 1 dia
-    print(resp)
-    return resp
+    if produto:
+        valor = f"Cód. Do produto: {produto.id}, nome: {produto.nome}, preco: {produto.preco}, img: {produto.img}"
+        resp = make_response("Cookie has been set!")
+        resp.set_cookie('car', valor, max_age=60 * 60 * 24)  # 1 dia
+        return resp
 
 @prod.route('/get_cookie')
 def get_cookie():
