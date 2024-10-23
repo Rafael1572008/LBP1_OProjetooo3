401e0964c05802da1d2cb62a8cb805fa1ab2b61c
Rafael Pires Gonçalves <127984148+Rafael1572008@users.noreply.github.com>
2024-10-22 21:57:14 -0300

    Tentativa de cookie

--- controllers/carrinho.py
+++ controllers/carrinho.py
@@ -1,21 +1,44 @@
-from flask import Blueprint, render_template, session, request, url_for, redirect
-from models.produtos import produtos   #importaçãoes
+from flask import Blueprint, render_template, session, request, url_for, redirect, make_response
+from models.produtos import produtos   # Importações
 
 prod = Blueprint('produtos', __name__)
 
 @prod.route('/carrinho')
 def carr():
-    return render_template('carrinho.html', produtos = produtos)
+    return render_template('carrinho.html', produtos=produtos)
 
-@prod.route('/carrinho/add', methods = ['POST', 'GET'])
+@prod.route('/carrinho/add', methods=['POST', 'GET'])
 def add():
     if request.method == 'POST':
-        r = request.form.get('produto_id')
-
-    if request.method == 'GET':
-        r = request.args.get('produto_id')
+        produto_id = request.form.get('produto_id')
+    elif request.method == 'GET':
+        produto_id = request.args.get('produto_id')
 
     for i in produtos:
-        print (f"Produtos(id={i.id}, nome={i.nome}, preco={i.preco}, img={i.img})")
+        print(f"Produtos(id={i.id}, nome={i.nome}, preco={i.preco}, img={i.img})")
 
-    return f"O produto tem indice {r}"    
+    return redirect(url_for('produtos.carr'))
+
+@prod.route('/set_cookie')
+def cookie(produto_id):
+
+    for i in produtos:
+        if i.id == produto_id:
+            nome = i.nome
+            preco = i.preco
+            img = i.img
+    
+    valor = f"Cód. Do produto: {produto_id}, nome: {nome}, preco: {preco}, img: {img}"
+
+    resp = make_response("Cookie has been set")
+    resp.set_cookie('car', valor, max_age=60*60*24)  # 1 dia
+    print(resp)
+    return resp
+
+@prod.route('/get_cookie')
+def get_cookie():
+    cookie_value = request.cookies.get('car')
+
+    if cookie_value:
+        return f"The cookie value is: {cookie_value}"
+    return "Cookie not found!"
