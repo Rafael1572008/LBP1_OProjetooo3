3f4a684df3a05809896df94edbbaf2fefd08cb3e
Rafael Pires Gonçalves <127984148+Rafael1572008@users.noreply.github.com>
2024-10-31 17:36:23 -0300

    Finalização do carrinho... Eu acho

--- controllers/carrinho.py
+++ controllers/carrinho.py
@@ -1,46 +1,53 @@
-from flask import Blueprint, render_template, request, url_for, redirect, make_response
+from flask import Blueprint, render_template, request, redirect, url_for, make_response, flash
 from models.produtos import produtos  # Importações
 
 prod = Blueprint('produtos', __name__)
 
 @prod.route('/carrinho')
-def carr():
-    return render_template('carrinho.html', produtos=produtos)
+def carrinho():
+    # Cria um dicionário para armazenar os produtos no carrinho com base nos cookies
+    carrinho = {}
+    for produto in produtos:
+        quantidade = request.cookies.get(f'produto_{produto.id}', 0)
 
-@prod.route('/carrinho/add', methods=['POST', 'GET'])
+        if int(quantidade) > 0:
+            total = produto.preco * int(quantidade)
+            carrinho[produto.id] = {'nome': produto.nome, 'qntd': int(quantidade), 'total': total}
+
+    return render_template('carrinho.html', produtos=produtos, carrinho=carrinho)
+
+@prod.route('/carrinho/add', methods=['GET', 'POST'])
 def add():
-    if request.method == 'POST':
-        produto_id = request.form.get('produto_id')
-    elif request.method == 'GET':
-        produto_id = request.args.get('produto_id')
+    if request.method == 'GET':
+        produto_id = request.args.get('id')
+    elif request.method == 'POST':
+        produto_id = request.form.get('id')
+    else:
+        print("oxi")
 
-    # Redireciona para a rota /set_cookie com o ID do produto
-    return redirect(url_for('produtos.set_cookie', produto_id= produto_id))
+    resp = make_response(redirect(url_for('produtos.carrinho')))
+    cookie = request.cookies.get(f'produto_{produto_id}')
 
-@prod.route('/set_cookie/<int:produto_id>')
-def set_cookie(produto_id):
-    # Busca o produto na lista de produtos
-    for i in produtos:
-        if i.id == produto_id:
-            produto = i
-            break 
+    
+    # Incrementa a quantidade do produto no cookie
+    if cookie:
+        resp.set_cookie(f'produto_{produto_id}', str(int(cookie) + 1))
+    else:
+        resp.set_cookie(f'produto_{produto_id}', "1")
+    
+    return resp
 
-    if produto:
-        valor = f"Cód. Do produto: {produto.id}, nome: {produto.nome}, preco: {produto.preco}, img: {produto.img}"
-        resp = make_response("Cookie has been set!")
-        resp.set_cookie('car', valor, max_age = 60 * 60 * 24 * 7)  # 1 semana
-        return resp
-
-@prod.route('/get_cookie')
-def get_cookie():
-    cookie_value = request.cookies.get('car')
-
-    if cookie_value:
-        return f"The cookie value is: {cookie_value}"
-    return "Cookie not found!"
-
-@prod.route('/del_cookie')
-def del_cookie():
-    resp = make_response("Cookie has been delet!")
-    resp.set_cookie('car', '', max_age = 0)  # deletar
+@prod.route('/carrinho/del', methods=['GET'])
+def delete():
+    produto_id = request.args.get('id')
+    resp = make_response(redirect(url_for('produtos.carrinho')))
+    cookie = request.cookies.get(f'produto_{produto_id}')
+    
+    # Decrementa a quantidade ou remove o cookie
+    if cookie:
+        if int(cookie) - 1 > 0:
+            resp.set_cookie(f'produto_{produto_id}', str(int(cookie) - 1))
+        else:
+            resp.set_cookie(f'produto_{produto_id}', '', expires=0)  # Remove o cookie
+    
     return resp
